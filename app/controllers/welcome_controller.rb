0536bf13351f3516e5fce77ed0b76b95bcb27ffc
rabiayilmazqwe@gmail.com <rabiayilmazqwe@gmail.com>
2024-05-31 01:46:11 +0300

    comment lines

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     if Article.exists? # herhangi bir kayıt olup olmadığını kontrol eder
-      redirect_to articles_path
+      redirect_to articles_path # eğer yukarıdaki true dönerse bu satır çalışır ve article path e gider 
+                                # article path articles olduğu yoldur 
     end
   end
 end
